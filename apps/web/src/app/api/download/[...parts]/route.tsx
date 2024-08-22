dd7bd2b89f846cb125492aaa95a4f14790993bd3
Hernán Patrón Costas <hernan@patroncostas.com>
2024-08-22 15:56:55 -0600

    fix: file pdf

---
+++ apps/web/src/app/api/download/[...parts]/route.tsx
@@ -0,0 +1,24 @@
+import { getFile } from '@documenso/lib/universal/upload/get-file';
+
+export const dynamic = 'force-dynamic'; // defaults to auto
+
+export async function GET(request: Request, { params }: { params: { parts: string[] } }) {
+  const file = await getFile({
+    type: 'S3_PATH',
+    data: params?.parts?.join('/'),
+  });
+  // await generateContracts();
+  // await writeFile(`${id}.pdf`, buffer);
+
+  return new Response(file, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/pdf',
+      'Content-disposition': 'inline',
+      'Content-Length': file.length,
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': '*',
+    },
+  });
+}
